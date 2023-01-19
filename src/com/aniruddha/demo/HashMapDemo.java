ae6eceba4e48b3d507476c34051606ff1546d3c8
Aniruddha <aniruddhakamble805@gmail.com>
2023-01-19 19:20:59 +0530

    Demo Project

---
+++ src/com/aniruddha/demo/HashMapDemo.java
@@ -0,0 +1,9 @@
+package com.aniruddha.demo;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Aniruddha");
+	}
+
+}
---
+++ src/com/aniruddha/demo/HashSetDemo.java
@@ -0,0 +1,19 @@
+package com.aniruddha.demo;
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+		Set<Integer> set=new HashSet<Integer>();
+		set.add(1);
+		set.add(2);
+		set.add(3);
+		set.add(4);
+		set.add(5);
+		for(Integer i: set) {
+			System.out.println(i);
+		}
+	}
+
+}

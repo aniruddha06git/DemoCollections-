826158a0e3f0ac13d350a47bcb973744af59a3f3
Aniruddha <aniruddhakamble805@gmail.com>
2023-01-19 19:32:45 +0530

    new classes added

---
+++ src/com/aniruddha/demo/ArrayListDemo.java
@@ -0,0 +1,10 @@
+package com.aniruddha.demo;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Aniruddha");
+
+	}
+
+}
--- src/com/aniruddha/demo/HashMapDemo.java
+++ src/com/aniruddha/demo/HashMapDemo.java
@@ -1,9 +1,15 @@
 package com.aniruddha.demo;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HashMapDemo {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Aniruddha");
+		Map<Integer,String> map=new HashMap<Integer,String>();
+		map.put(101,"Aniruddha");
+		map.put(102,"Samrat");
+		map.put(103,"Shakya");
+		System.out.println(map);
 	}
 
 }
